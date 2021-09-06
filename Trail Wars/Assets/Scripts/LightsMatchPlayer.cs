4e22cce36ffe2062e5963dba432455c40c6f0bf1
NoCereaL <therealskm@hotmail.com>
2021-09-06 14:32:33 +0100

    Light Matches Player

---
+++ Trail Wars/Assets/Scripts/LightsMatchPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightsMatchPlayer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Light2D light2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        light2D = this.gameObject.GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light2D.color = spriteRenderer.color;
+    }
+}
