82420e570c9a369f8d4ae820157caf29eb7c01b5
NoCereaL <therealskm@hotmail.com>
2021-09-06 16:27:07 +0100

    Complete Shapes Store

--- Trail Wars/Assets/Scripts/CurrencyScripts/CheckShape.cs
+++ Trail Wars/Assets/Scripts/CurrencyScripts/CheckShape.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckShape : MonoBehaviour
 {
-    public GameObject itemName;
-    public GameObject owned;
+    //public GameObject itemName;
+
+    public string itemName;
+    public int itemAmount;
+    public GameObject lockIcon;
+    public Button button;
+    public Text amountText;
+    public GameObject DiamondImg;
+    public GameObject Text;
     public GameObject equipped;
+    public GameObject owned;
+
+    public EquipColor item;
+    public EquipShape itemShape;
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemName = this.gameObject.name;
+        button = this.gameObject.GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (int.Parse(itemName.name) == PlayerPrefs.GetInt("PlayerShape"))
+        amountText.text = itemAmount + "";
+        ToBuy();
+    }
+
+    public void ToBuy()
+    {
+        if (PlayerPrefs.GetInt("Diamond") >= itemAmount)       //if you have enough money you can buy
+        {
+            button.interactable = true;
+        }
+        if (PlayerPrefs.GetInt("Diamond") <= itemAmount && PlayerPrefs.GetInt(itemName) == 0)
+        {
+            button.interactable = false;
+        }
+        if (PlayerPrefs.GetInt(itemName) == 1)          //if you own then you can equip
+        {
+            button.interactable = true;
+            lockIcon.SetActive(false);
+            DiamondImg.SetActive(false);
+            Text.SetActive(false);
+            owned.SetActive(true);
+        }
+        if (int.Parse(itemName) == PlayerPrefs.GetInt("PlayerShape"))
         {
             owned.SetActive(false);
             equipped.SetActive(true);
+        }
+        else
+        {
+            equipped.SetActive(false);
         }
     }
 }
