cbb71b47e89149c8f02324bfe7c06e7f38413f4f
NoCereaL <therealskm@hotmail.com>
2021-09-09 10:45:46 +0100

    Update

---
+++ Trail Wars/Assets/Scripts/NormalMode/ObstacleGate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGate : MonoBehaviour
+{
+    public GameObject obstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacle = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            obstacle.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+        else
+        {
+            obstacle.GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+    }
+}
